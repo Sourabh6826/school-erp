3cb7ac29581a412eb66f77a346800b512a68e9ec
Sourabh Mishra <soravmishra@gmail.com>
2026-01-26 00:40:00 +0530

    Update settings.py

--- Backend/school_erp/settings.py
+++ Backend/school_erp/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('RENDER_EXTERNAL_HOSTNAME', '*'), # '*' is a quick fix, but specific is better
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
